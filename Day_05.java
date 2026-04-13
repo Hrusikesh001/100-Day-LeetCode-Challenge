80dc48d397605516b4341e1adf8bf97a07c718fd
Hrusikesh123 <hs2068672@gmail.com>
2026-04-14 03:12:20 +0530

    feat: add solution for Day 05 water bottles problem

---
+++ Day_05.java
@@ -0,0 +1,21 @@
+// There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+// The operation of drinking a full water bottle turns it into an empty bottle.
+// Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+public class Day_05 {
+    public static void main(String[] args) {
+        int numBottles = 15;
+        int numExchange = 4;
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            int newBottles = numBottles / numExchange;
+            int emptyBottles = numBottles % numExchange;
+            numBottles = newBottles + emptyBottles;
+            ans = ans + newBottles;
+            
+            
+        }
+        System.out.println(ans);
+    
+    }
+}
